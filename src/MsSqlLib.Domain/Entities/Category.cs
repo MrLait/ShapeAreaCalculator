26fa0f3b86ed8a712036033e10de077ab4b20811
Lait <mr.lait3@gmail.com>
2024-04-06 18:09:06 +0300

    #3 add entities: product and category

---
+++ src/MsSqlLib.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace MsSqlLib.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        //Many-to-many
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ src/MsSqlLib.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MsSqlLib.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        //Many-to-many
+        public ICollection<Category>? Categories { get; set; }
+    }
+}
